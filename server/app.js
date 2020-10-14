65394ca97ec6a6250607bfcaba8280d35df7a9ce
ThalesAvila <dev.thales.avila@gmail.com>
2020-10-14 13:55:24 -0300

    Configurando Graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: "",
+  })
+);
 
 app.listen(4000, () => {
   console.log("Listening on port 4000");
